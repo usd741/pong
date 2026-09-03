21f68b2f218058f2b8db8d095ab659bb5b3f6f87
ibra <unclesam523@gmail.com>
2026-09-03 17:57:32 +0500

    2026.09.03

--- components/ball_spawner_component.gd
+++ components/ball_spawner_component.gd
@@ -1,12 +1,31 @@
 class_name BallSpawnerComponent
 extends Node
 
+signal ball_scored(side: int) #1 = right, 2 = left
+
+@export var ball_scene: PackedScene #select ball scene
+@export var spawn_position: Vector2 = Vector2(320, 240)
+
+var ball: RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	spawn_ball()
+
+func spawn_ball():
+	if ball:
+		ball.queue_free()
+	
+	ball = ball_scene.instantiate()
+	add_child(ball)
+	ball.global_position = spawn_position
+	ball.ball_exited_screen.connect(_on_ball_exited)
+	
+	if ball.has_method("prepare_and_shoot"):
+		ball.prepare_and_shoot()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_ball_exited():
+	var side = 1 if ball.global_position.x > 0 else 2
+	ball_scored.emit(side)
+	spawn_ball()
--- scripts/round_game_manager.gd
+++ scripts/round_game_manager.gd
@@ -3,54 +3,36 @@ extends Node
 var win_score: int = 5
 
 @export var ui_scene: Node #select the ui scene
-@export var ball_scene: PackedScene #select ball scene
-
+@export var ball_spawner: BallSpawnerComponent
 
 @onready var score_label_p1 = ui_scene.get_node("%LabelP1Score")
 @onready var score_label_p2 = ui_scene.get_node("%LabelP2Score")
 
-
 #current round scores
 var round_score_p1: int = 0
 var round_score_p2: int = 0
-var ball: RigidBody2D
+
+
 
 func _ready() -> void:
 	if ui_scene:
 		score_label_p1 = ui_scene.get_node("%LabelP1Score")
 		score_label_p2 = ui_scene.get_node("%LabelP2Score")
-	
-	#spawning first ball on the scene
-	if ball_scene:
-		ball = ball_scene.instantiate()
-		add_child(ball)
-		ball.global_position = Vector2(320, 240)
-		ball.ball_exited_screen.connect(_on_ball_exited)
-		#call shoot function, if it exist in ball script
-		if ball.has_method("prepare_and_shoot"):
-			ball.prepare_and_shoot()
+	if ball_spawner:
+		ball_spawner.ball_scored.connect(_on_ball_exited)
 
-func _on_ball_exited() -> void:
-	#allocating ball's side position
-	if ball.global_position.x < 0:
-		print("Мяч улетел влево: +очко Игроку №2")
-		round_score_p2 += 1
-	else:
+
+
+func _on_ball_exited(side: int) -> void:
+	if side == 1:
 		print("Мяч улетел вправо: +очко Игроку №1")
 		round_score_p1 += 1
+	else:
+		print("Мяч улетел влево: +очко Игроку №2")
+		round_score_p2 += 1
 	update_score_ui()
 	check_round_end()
-	reset_ball()
 
-func reset_ball():
-	print("Reset ball starts")
-	ball.queue_free()
-	var new_ball =ball_scene.instantiate()
-	add_child(new_ball)
-	new_ball.global_position = Vector2(320, 240)
-	ball = new_ball
-	ball.ball_exited_screen.connect(_on_ball_exited)
-	ball.prepare_and_shoot()
 
 func update_score_ui() -> void:
 	score_label_p1.text = str(round_score_p1)
@@ -61,8 +43,3 @@ func check_round_end():
 		GlobalGameManager.add_round_result(1, round_score_p1, round_score_p2)
 	elif round_score_p2 >= win_score:
 		GlobalGameManager.add_round_result(2, round_score_p1, round_score_p2)
-		
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
